df3aa4ec9d0e9a98efc203027d7381c5d90851b4
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-10-27 06:21:00 +0000

    fine esercizio

--- app.py
+++ app.py
@@ -1,52 +1,79 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
+import io
+import pandas as pd 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import pymssql
 
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def home():
-    return render_template('radio.html')
-
+    return render_template("home.html")
 
 @app.route("/select", methods=["GET"])
-def elenco():
-    scelta= request.args["scelta"]
+def scelta():
+    global tabella, sceltaUtente
+    sceltaUtente = request.args["scelta"]
+    print(sceltaUtente)
+    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='giurato.fabrizio', password='xxx123##', database='giurato.fabrizio')
+    if sceltaUtente == "es1":
+        query = 'SELECT category_name,count(*) as numero_prodotti FROM production.categories INNER JOIN production.products ON categories.category_id = products.category_id GROUP BY category_name'
+        tabella = pd.read_sql(query,conn)
+        tabella.sort_values(by='numero_prodotti',ascending=False,inplace=True)
 
-    if scelta == "es1":
-        return redirect(url_for("es1"))
-    elif scelta == "es2":
-        return render_template("es2.html")
-    elif scelta == "es3":
-        return render_template("es3.html")
-    elif scelta == "es4":
+    elif sceltaUtente == "es2":
+        query = 'SELECT store_name,count(order_id) as numero_ordini FROM sales.orders INNER JOIN sales.stores ON orders.store_id = stores.store_id GROUP BY store_name'
+        tabella = pd.read_sql(query,conn)
+        tabella.sort_values(by='numero_ordini',ascending=False,inplace=True)
+        
+    elif sceltaUtente == "es3":
+        query = 'SELECT brand_name,count(*) as numero_prodotti FROM production.products INNER JOIN production.brands ON products.brand_id = brands.brand_id GROUP BY brand_name'
+        tabella = pd.read_sql(query,conn)
+        tabella.sort_values(by='numero_prodotti',ascending=False,inplace=True)
+
+    else:
         return render_template("search.html")
 
+    return render_template("result.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
+@app.route("/grafico.png", methods=["GET"])
+def visualizza():
+    if sceltaUtente == "es1":
+        fig = plt.figure()
+        ax = plt.axes()
+        ax.bar(tabella.category_name,tabella.numero_prodotti)
+        plt.xticks(rotation=90)
 
+    elif sceltaUtente == "es2":
+        fig = plt.figure()
+        ax = plt.axes()
+        ax.barh(tabella.store_name,tabella.numero_ordini)
+    else:
+        fig =  plt.figure(figsize=(8,8))
+        ax = plt.axes()
+        ax.pie(tabella.numero_prodotti,labels = tabella.brand_name)
+        fig.show()
 
-@app.route('/result', methods=['GET'])
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route("/result", methods=["GET"])
 def result():
-    # collegamento database
-    import pandas as pd
-    import pymssql
-    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user= 'giurato.fabrizio', password='xxx123##', database='giurato.fabrizio')
-
-    # invio query al database e ricezione informazioni
-    NomeProdotto = request.args['nomeProdotto']
-    query = f"select * from production.products where product_name like '{NomeProdotto}%'"
+    # Collegamento al database
+    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='porta.matteo', password='xxx123##', database='porta.matteo')
+    # Invio query al database e ricezione informazioni
+    NomeProdotto = request.args["NomeProdotto"]
+    query = f"SELECT * FROM production.products WHERE product_name LIKE '{NomeProdotto}%'" 
+    # Visualizzare le informazioni 
     dfProdotti = pd.read_sql(query,conn)
-
-    # far visualizare le informazioni all'utente
- 
-    return render_template('result.html', nomiColonne = dfProdotti.columns.values, dati = list(dfProdotti.values.tolist()))
-
-
-
-@app.route("/es1", methods=["GET"])
-def es1():
-    query = f'SELECT category_name, count(*) as numero_prodotti FROM production.products inner join production.categories on categories.category_id = products.category_id group by category_name'
-    dfCategorie = pd.read_sql(query,conn)
-    return render_template('es1.html', nomiColonne = dfCategorie.columns.values, dati = list(dfCategorie.values.tolist()))
+    return render_template("result_search.html", tabella = dfProdotti.to_html(), nomiColonne = dfProdotti.columns.values, dati = dfProdotti.values)
 
 
 if __name__ == '__main__':
